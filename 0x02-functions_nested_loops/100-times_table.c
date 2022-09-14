6b3909abb9050698ef8a06d5f704773706cf2e97
Talilio <philibertvona@gmail.com>
2022-09-14 11:05:33 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,48 +6,43 @@
  */
 void print_times_table(int n)
 {
-int num1, num2, num3, num4, num5, num6;
-
+int num1, num2;
 if (n >= 0 && n < 15)
 {
 for (num1 = 0; num1 <= n; num1++)
 {
 for (num2 = 0; num2 <= n; num2++)
 {
-num3 = num1 * num2;
-num4 = num3 / 100;
-num5 = (num3 / 10) % 10;
-num6 = (num3 % 100) % 10;
-if (num2 == 0)
+if ((num1 * num2) < 10)
 {
-_putchar('0');
-}
-else if (num3 < 10)
+if (num2 != 0)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar('0' + num6);
-}
-else if (num3 < 100)
-{
-_putchar(' ');
-_putchar('0' + num5);
-_putchar('0' + num6);
-}
-else
-{
-_putchar('0' + num4);
-_putchar('0' + num5);
-_putchar('0' + num6);
-}
-if (num2 < n)
-{
-_putchar(',');
 _putchar(' ');
 }
-else
+_putchar((num2 * num1) + '0');
+}
+else if ((num1 * num2) < 100)
+{
+_putchar(' ');
+_putchar((((num2 * num1) / 10) % 10) + '0');
+_putchar((((num2 * num1) % 10) + '0'); 
+}
+else if ((num1 * num2) < 1000)
+{
+_putchar(' ');
+_putchar((((num2 * num1) / 100) % 10) + '0');
+_putchar((((num2 * num1) / 10) % 10) + '0');
+_putchar(((num2 * num1) % 10) + '0');
+}
+if (num2 != (n))
+{
+_putchar(' ');
+}
+}
 _putchar('\n');
 }
 }
 }
-}
+
