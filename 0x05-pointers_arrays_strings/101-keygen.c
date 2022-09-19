5a4b68b347d8583a2cabbe7d241d8fa3a340a748
Talilio <philibertvona@gmail.com>
2022-09-19 10:55:25 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,14 +4,29 @@
 #include "stdio.h"
 
 /**
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 {
-int a;
+int i, sum, n;
+int pass[100];
 
-srand(time(0));
-a = rand ();
-printf("%i\n", a);
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
 return (0);
 }
