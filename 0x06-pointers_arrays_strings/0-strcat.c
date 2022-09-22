7820ba438db852ce5267dfc0275880600e8da63f
Talilio <philibertvona@gmail.com>
2022-09-22 02:09:16 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,18 +12,18 @@ char *_strcat(char *dest, char *src)
 int a, b;
 
 a = 0;
-while (dest[a] != '\0')
-{
-a++;
-}
+b = 0;
 
-b = 0
+while (dest[a] != '\0')
+a++;
+
 while (src[b] != '\0')
 {
 dest[a] = src[b];
 b++;
 a++;
 }
+
 dest[a] = '\0';
 
 return (dest);
