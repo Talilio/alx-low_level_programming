182912e8d99c71699d6f5ad2065ace99eba26a6f
Talilio <philibertvona@gmail.com>
2022-09-30 03:19:08 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,9 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void) argc;
-
-	printf("%s\n", argv[0])
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
