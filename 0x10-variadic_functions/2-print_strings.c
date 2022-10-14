156f6d07c41dc9f35498f4fb91a5a0ce798cf9ff
Talilio <philibertvona@gmail.com>
2022-10-14 07:33:51 -0700

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,9 +26,4 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (i < n - 1)
 			if (separator)
-				printf("%s", separator);
-	}
-	printf("\n");
-	va_end(valist);
-}
-				
+
